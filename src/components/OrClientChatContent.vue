0b1a587d7eaaa0c6c9f63da335336fdc90935914
standstonecraft <stand.stone.comlex@gmail.com>
2024-07-10 09:46:18 +0900

    split structured content

--- src/components/OrClientChatContent.vue
+++ src/components/OrClientChatContent.vue
@@ -2,20 +2,8 @@
   <div class="d-flex flex-column pa-3" :class="props.enabled ? '' : 'text-disabled'">
     <!-- display -->
     <span class="text-subtitle-1 font-weight-bold">{{ props.role }}({{ agentName }}):</span>
-    <p v-if="structuredContent?.length < 2" v-html="parsedContent" class="pl-4">
-    </p>
-    <v-expansion-panels v-else model-value="answer" multiple>
-      <v-expansion-panel v-for="item in structuredContent" :key="item.value" :title="item.title" :value="item.value">
-        <v-expansion-panel-text>
-          <p v-html="item.text"></p>
-        </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <v-btn-toggle model-value="answer" multiple>
-      <v-btn v-for="item in structuredContent" :key="item.value" :value="item.value">
-        <span>{{ item.title }}</span>
-      </v-btn>
-    </v-btn-toggle>
+    <OrClientChatContentStructured :raw-content="props.content" />
+    <!-- image -->
     <div v-if="props.contentType == 'image_url'">
       <div v-if="props.contentImage">
         <v-img :width="60" cover :src="props.contentImage"></v-img>
@@ -61,6 +49,7 @@ import { asyncComputed } from '@vueuse/core'
 import { Marked } from "marked";
 import markedCodeFormat from 'marked-code-format'
 import DOMPurify from 'dompurify';
+import OrClientChatContentStructured from './OrClientChatContentStructured.vue';
 
 const props = defineProps<ChatContent>();
 const emit = defineEmits<{
@@ -92,32 +81,6 @@ const parsedContent = asyncComputed(async () => {
   // return await marked.parse(props.content);
   return purify.sanitize(await marked.parse(props.content));
 });
-type ContentSection = {
-  title: string;
-  /** for model value of button group */
-  value: string;
-  text: string;
-}
-const structuredContent = asyncComputed(async () => {
-  if (!props.content) return [];
-  const intro = extractAndRemove(props.content, /<intro>(.*?)<\/intro>/s);
-  intro[0] = purify.sanitize(await marked.parse(intro[0])).trim();
-  const thinking = extractAndRemove(intro[1], /<thinking>(.*?)<\/thinking>/s);
-  thinking[0] = purify.sanitize(await marked.parse(thinking[0])).trim();
-  const answer = extractAndRemove(thinking[1], /<answer>(.*?)<\/answer>/s);
-  answer[0] = purify.sanitize(await marked.parse(answer[0])).trim();
-  const outro = extractAndRemove(answer[1], /<outro>(.*?)<\/outro>/s);
-  outro[0] = purify.sanitize(await marked.parse(outro[0])).trim();
-  const ret: ContentSection[] = [
-    { title: "INTRO", value: "intro", text: intro[0] },
-    { title: "THINKING", value: "thinking", text: thinking[0] },
-    { title: "ANSWER", value: "answer", text: answer[0] },
-    { title: "OUTRO", value: "outro", text: outro[0] },
-    { title: "OTHER", value: "other", text: outro[1]?.trim() },
-  ]
-    .filter(x => x.text);
-  return ret;
-})
 /**
  * 有効状態を切り替える
  * 
@@ -126,15 +89,5 @@ const structuredContent = asyncComputed(async () => {
  */
 function toggleEnabled() {
   store.contents.update({ ...props, enabled: !props.enabled });
-}
-
-function extractAndRemove(text: string, pattern: RegExp): [string, string] {
-  const match = text.match(pattern);
-  if (match) {
-    const extracted = match[0];
-    const remaining = text.replace(pattern, '');
-    return [extracted, remaining];
-  }
-  return ['', text];
 }
 </script>
---
+++ src/components/OrClientChatContentStructured.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <v-list v-model:opened="structuredContent.open">
+      <v-list-group v-for="subContent in structuredContent.subContents" :key="subContent.value">
+        <template v-slot:activator="{ props }">
+          <v-list-item v-bind="props" prepend-icon="mdi-account-circle" :title="subContent.title"></v-list-item>
+        </template>
+        <p v-html="subContent.text" class="pl-4"></p>
+      </v-list-group>
+    </v-list>
+    <p v-html="structuredContent.remaining" class="pl-4"></p>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computedAsync } from '@vueuse/core';
+import DOMPurify from 'dompurify';
+import { Marked } from 'marked';
+import markedCodeFormat from 'marked-code-format';
+
+const props = defineProps({
+  rawContent: { type: String, required: true }
+});
+
+
+const structuredContent = computedAsync<{
+  subContents: {
+    title: string;
+    value: string;
+    text: string;
+  }[];
+  remaining: string;
+  open: string[];
+}>(async () => {
+  if (!props.rawContent) return { subContents: [], remaining: props.rawContent, open: [] };
+  const intro = extractAndRemove(props.rawContent, /<intro>(.*?)<\/intro>/s);
+  intro[0] = (await marked.parse(intro[0])).trim();
+  const thinking = extractAndRemove(intro[1], /<thinking>(.*?)<\/thinking>/s);
+  thinking[0] = (await marked.parse(thinking[0])).trim();
+  const answer = extractAndRemove(thinking[1], /<answer>(.*?)<\/answer>/s);
+  answer[0] = (await marked.parse(answer[0])).trim();
+  const outro = extractAndRemove(answer[1], /<outro>(.*?)<\/outro>/s);
+  outro[0] = (await marked.parse(outro[0])).trim();
+  const ret = {
+    subContents: [
+      { title: "INTRO", value: "intro", text: intro[0] },
+      { title: "THINKING", value: "thinking", text: thinking[0] },
+      { title: "ANSWER", value: "answer", text: answer[0] },
+      { title: "OUTRO", value: "outro", text: outro[0] },
+    ].filter(x => x.text),
+    remaining: (await marked.parse(outro[1])).trim(),
+    open: answer[0] ? ["answer"] : []
+  };
+  return ret;
+}, { subContents: [], remaining: "", open: [] }, { lazy: true });
+
+/**
+ * retrieve and remove matched text
+ * @param text target text
+ * @param pattern regex pattern
+ * @returns [extracted text, remaining text]
+ */
+function extractAndRemove(text: string, pattern: RegExp): [string, string] {
+  const match = text.match(pattern);
+  if (match) {
+    const extracted = match[0];
+    const remaining = text.replace(pattern, '');
+    return [extracted, remaining];
+  }
+  return ['', text];
+}
+
+// sanitize
+const marked = new Marked()
+  .use(
+    markedCodeFormat({
+      /* Prettier options */
+    })
+  )
+const purify = DOMPurify(window);
+</script>
