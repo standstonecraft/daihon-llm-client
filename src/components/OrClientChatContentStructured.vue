2f055fde64cef26e2ff3acbc741b0ff79da846c8
standstonecraft <stand.stone.comlex@gmail.com>
2024-07-10 15:06:36 +0900

    modify structured content

--- src/components/OrClientChatContentStructured.vue
+++ src/components/OrClientChatContentStructured.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <v-list v-model:opened="structuredContent.open">
-      <v-list-group v-for="subContent in structuredContent.subContents" :key="subContent.value">
-        <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-account-circle" :title="subContent.title"></v-list-item>
-        </template>
-        <p v-html="subContent.text" class="pl-4"></p>
-      </v-list-group>
-    </v-list>
+    <div v-for="subContent in structuredContent.subContents" :key="subContent.value">
+      <!-- <v-btn :append-icon="listOpened.includes(subContent.value) ? '$collapse' : '$expand'" :text="subContent.title"
+        @click="toggleList(subContent.value)" variant="text"></v-btn> -->
+      <div class="cursor-pointer" @click="toggleList(subContent.value)">
+        <span class="text-capitalize text-subtitle">{{ subContent.title }}</span>
+        <v-icon>{{ listOpened.includes(subContent.value) ? '$collapse' : '$expand' }}</v-icon>
+      </div>
+      <v-expand-transition>
+        <div v-show="listOpened.includes(subContent.value)" v-html="subContent.text" class="pl-6">
+        </div>
+      </v-expand-transition>
+    </div>
     <p v-html="structuredContent.remaining" class="pl-4"></p>
   </div>
 </template>
@@ -20,7 +24,6 @@ const props = defineProps({
   rawContent: { type: String, required: true }
 });
 
-
 const structuredContent = computedAsync<{
   subContents: {
     title: string;
@@ -28,29 +31,39 @@ const structuredContent = computedAsync<{
     text: string;
   }[];
   remaining: string;
-  open: string[];
 }>(async () => {
   if (!props.rawContent) return { subContents: [], remaining: props.rawContent, open: [] };
-  const intro = extractAndRemove(props.rawContent, /<intro>(.*?)<\/intro>/s);
+  const intro = extractAndRemove(props.rawContent, /<llm_intro>(.*?)<\/llm_intro>/s);
   intro[0] = (await marked.parse(intro[0])).trim();
-  const thinking = extractAndRemove(intro[1], /<thinking>(.*?)<\/thinking>/s);
-  thinking[0] = (await marked.parse(thinking[0])).trim();
-  const answer = extractAndRemove(thinking[1], /<answer>(.*?)<\/answer>/s);
+  const answer = extractAndRemove(intro[1], /<llm_answer>(.*?)<\/llm_answer>/s);
   answer[0] = (await marked.parse(answer[0])).trim();
-  const outro = extractAndRemove(answer[1], /<outro>(.*?)<\/outro>/s);
+  const outro = extractAndRemove(answer[1], /<llm_outro>(.*?)<\/llm_outro>/s);
   outro[0] = (await marked.parse(outro[0])).trim();
   const ret = {
     subContents: [
       { title: "INTRO", value: "intro", text: intro[0] },
-      { title: "THINKING", value: "thinking", text: thinking[0] },
       { title: "ANSWER", value: "answer", text: answer[0] },
       { title: "OUTRO", value: "outro", text: outro[0] },
     ].filter(x => x.text),
-    remaining: (await marked.parse(outro[1])).trim(),
-    open: answer[0] ? ["answer"] : []
+    remaining: (await marked.parse(outro[1])).trim()
   };
   return ret;
-}, { subContents: [], remaining: "", open: [] }, { lazy: true });
+}, { subContents: [], remaining: "" }, { lazy: true });
+
+const listOpened = ref<string[]>([]);
+function toggleList(value: string) {
+  if (listOpened.value.includes(value)) {
+    listOpened.value = listOpened.value.filter(x => x !== value);
+  } else {
+    listOpened.value.push(value);
+  }
+}
+watch(structuredContent, () => {
+  const ret: string[] = [];
+  if (structuredContent.value.subContents.map(x => x.value).includes("answer")) ret.push("answer");
+  if (structuredContent.value.subContents.map(x => x.value).includes("question")) ret.push("question");
+  listOpened.value = ret;
+})
 
 /**
  * retrieve and remove matched text
